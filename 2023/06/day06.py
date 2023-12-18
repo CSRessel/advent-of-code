ad1a474723df6945d890df569e39541361f0871c
Clifford Ressel <CSRessel@gmail.com>
2023-12-18 15:17:53 -0500

    scaffold day 06

---
+++ 2023/06/day06.py
@@ -0,0 +1,67 @@
+import functools
+import itertools
+import json
+import math
+import re
+import sys
+from functools import cache
+
+from collections import namedtuple, deque, defaultdict
+from collections.abc import Callable
+from pathlib import Path
+from typing import Iterable, List, Optional, Literal
+
+
+def find_idx_ele[T](iterable: Iterable[T], predicate: Callable[[T], bool]) -> Optional[tuple[int, T]]:
+    def predicate_tuple(tup: tuple[int, T]) -> bool:
+        _, element = tup
+        return predicate(element)
+
+    idxele: Optional[tuple[int, T]] = next(filter(predicate_tuple, enumerate(iterable)), None)
+    return idxele
+
+
+def find_idx[T](iterable: Iterable[T], predicate: Callable[[T], bool]) -> Optional[int]:
+    result = find_idx_ele(iterable, predicate)
+    return result[0] if result is not None else result  # rip option monad not in python ; w ;
+
+
+def find_ele[T](iterable: Iterable[T], predicate: Callable[[T], bool]) -> Optional[T]:
+    result = find_idx_ele(iterable, predicate)
+    return result[1] if result is not None else result  # rip option monad not in python ; w ;
+
+
+# This results in the parent directory of the script
+SCRIPT_DIR = Path(__file__).absolute().parent
+DEBUG = False
+TEST = """Time:      7  15   30
+Distance:  9  40  200
+"""
+
+
+def part1(input: str):
+    times, distances = [list(map(int, line[len("distance: ") :].split())) for line in input.splitlines()]
+
+    print(f"{times}, {distances}")
+    return 0
+
+
+def part2(input: str):
+    return 0
+
+
+def main():
+    print("Part 1")
+    print("Test: ", test1 := part1(TEST))
+    assert test1 == 0
+    # print("Answer: ", answer1 := part1(open(SCRIPT_DIR / "input").read()))
+    # assert answer1 == 0
+    # print("Part 2")
+    # print("Test: ", test2 := part2(TEST))
+    # assert test2 == 0
+    # print("Answer: ", answer2 := part2(open(SCRIPT_DIR / "input").read()))
+    # assert answer2 == 0
+
+
+if __name__ == "__main__":
+    main()
